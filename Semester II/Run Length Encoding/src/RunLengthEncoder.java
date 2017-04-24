d0d4395a18c4c70f8a26e62d037bd4452fa6c4f4
Abner B <jorgeabner2000@gmail.com>
2017-04-24 09:17:28 -0700

    Update

--- Semester II/Run Length Encoding/src/RunLengthEncoder.java
+++ Semester II/Run Length Encoding/src/RunLengthEncoder.java
@@ -1,7 +1,18 @@
-
+import java.io.*;
 
 public class RunLengthEncoder 
 {
-	
-	
+	public static void main (String[] args)
+	{
+		char currentChar, nextChar;
+		
+		BufferedReader br = new BufferedReader(new File("RLEtest"));
+		FileWriter fw = new FileWriter("outputEncoder");
+		
+		
+		while(br.ready())
+		{
+			
+		}
+	}
 }
